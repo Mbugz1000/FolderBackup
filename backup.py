88336d1f6339772f8a08e635a361d5b8f1294936
KTMBUGUA <km.thairu@gmail.com>
2020-01-05 18:31:35 +0300

    Completed Program

--- backup.py
+++ backup.py
@@ -1,5 +1,54 @@
+import time
 from distutils.dir_util import copy_tree
+import shutil, os
+from pathlib import Path
+from datetime import datetime
+
+def copy_dir(src, dst):
+    dst.mkdir(parents=True, exist_ok=True)
+    for item in os.listdir(src):
+        s = src / item
+        d = dst / item
+        if s.is_dir():
+            copy_dir(s, d)
+        else:
+            shutil.copy2(str(s), str(d))
 
 
-# if __name__ == '__main___':
-copy_tree('C:\\Users\ktmbugua\\Documents\\Smart Analytics Role\\Networks App\\New Images\\dex2jar-2.0', 'C:\\Users\\ktmbugua\\Documents\\Smart Analytics Role\\Networks App\\New Images\\Test\\dex2jar-2.0')
+def log_event(event):
+    with open("log", "a") as text_file:
+        text_file.write("\n" + event)
+
+print('********************Folder Copier********************')
+
+while True:
+    command = input("\nCopy Files or Exit Program?")
+
+    if command != 'exit':
+        try:
+            with open("folder_backup", "r") as text_file:
+                folder_backups = text_file.read()
+
+            with open("folders", "r") as text_file:
+                folders = text_file.read()
+
+            directories = folders.split('\n')
+            # folder_locs =  [for dir in directories]
+
+            for dir in directories:
+                folder_name = dir.split('\\')[-1]
+                copy_dir(Path(dir), Path(folder_backups+'\\'+folder_name))
+                print('{}: Backed up {}'.format(datetime.now(), folder_name))
+
+            print('Succeded!')
+            print('-----------------------------------------------------------------------------------------------------------')
+
+
+        except Exception as e:
+            print('Unexpected Error Experienced!')
+            log_event('{}: {}'.format(datetime.now(), e))
+    else:
+        break
+
+print("\nExiting Program...")
+time.sleep(2)
