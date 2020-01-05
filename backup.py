435f0e468c9fc628024edafb52e8c2238a426217
KTMBUGUA <km.thairu@gmail.com>
2020-01-05 17:50:36 +0300

    Initial Commit

---
+++ backup.py
@@ -0,0 +1,5 @@
+from distutils.dir_util import copy_tree
+
+
+# if __name__ == '__main___':
+copy_tree('C:\\Users\ktmbugua\\Documents\\Smart Analytics Role\\Networks App\\New Images\\dex2jar-2.0', 'C:\\Users\\ktmbugua\\Documents\\Smart Analytics Role\\Networks App\\New Images\\Test\\dex2jar-2.0')
